ffff250d1dbbbafe999efb2dd57f29a4059230b0
ekrishgupta <ekrishgupta@users.noreply.github.com>
2026-03-05 19:31:59 -0800

    test(core): add TestPreviewPurge

---
+++ internal/core/cleaner_test.go
@@ -0,0 +1,35 @@
+package core
+
+import (
+	"os"
+	"testing"
+)
+
+func TestPreviewPurge(t *testing.T) {
+	tmpDir, err := os.MkdirTemp("", "navtex-test-*")
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer os.RemoveAll(tmpDir)
+
+	// Create some files
+	os.WriteFile(tmpDir+"/main.tex", []byte(""), 0o644)
+	os.WriteFile(tmpDir+"/main.aux", []byte(""), 0o644)
+	os.WriteFile(tmpDir+"/main.log", []byte(""), 0o644)
+	os.WriteFile(tmpDir+"/image.png", []byte(""), 0o644)
+
+	entries, err := PreviewPurge(tmpDir)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	// Should identify .aux and .log but not .tex or .png
+	if len(entries) != 2 {
+		t.Errorf("Expected 2 preview entries, got %d: %v", len(entries), entries)
+	}
+
+	// Verify files still exist
+	if _, err := os.Stat(tmpDir + "/main.aux"); os.IsNotExist(err) {
+		t.Error("Preview should not delete files")
+	}
+}
