b6af8d7b29f6ae9b25188aa40f85477e41d1aad2
ekrishgupta <ekrishgupta@users.noreply.github.com>
2026-03-05 19:28:02 -0800

    test(core): add TestScanDirectory_Source

--- internal/core/scanner_test.go
+++ internal/core/scanner_test.go
@@ -21,3 +21,25 @@ func TestScanDirectory_Empty(t *testing.T) {
 		t.Errorf("Expected 0 files, got %d", pf.Total())
 	}
 }
+
+func TestScanDirectory_Source(t *testing.T) {
+	tmpDir, err := os.MkdirTemp("", "navtex-test-*")
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer os.RemoveAll(tmpDir)
+
+	os.WriteFile(tmpDir+"/main.tex", []byte(""), 0o644)
+
+	pf, err := ScanDirectory(tmpDir)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	if len(pf.Source) != 1 {
+		t.Errorf("Expected 1 source file, got %d", len(pf.Source))
+	}
+	if pf.Source[0].Name != "main.tex" {
+		t.Errorf("Expected main.tex, got %s", pf.Source[0].Name)
+	}
+}
