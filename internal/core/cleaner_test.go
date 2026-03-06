89eefeb220896bfd0f6cc6563a00382e40a2edfb
ekrishgupta <ekrishgupta@users.noreply.github.com>
2026-03-05 19:32:14 -0800

    test(core): add TestPurge

--- internal/core/cleaner_test.go
+++ internal/core/cleaner_test.go
@@ -33,3 +33,30 @@ func TestPreviewPurge(t *testing.T) {
 		t.Error("Preview should not delete files")
 	}
 }
+
+func TestPurge(t *testing.T) {
+	tmpDir, err := os.MkdirTemp("", "navtex-test-*")
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer os.RemoveAll(tmpDir)
+
+	os.WriteFile(tmpDir+"/main.tex", []byte(""), 0o644)
+	os.WriteFile(tmpDir+"/main.aux", []byte(""), 0o644)
+
+	removed, err := Purge(tmpDir)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	if len(removed) != 1 || removed[0] != "main.aux" {
+		t.Errorf("Expected [main.aux] to be removed, got %v", removed)
+	}
+
+	if _, err := os.Stat(tmpDir + "/main.aux"); !os.IsNotExist(err) {
+		t.Error("File main.aux should have been deleted")
+	}
+	if _, err := os.Stat(tmpDir + "/main.tex"); os.IsNotExist(err) {
+		t.Error("File main.tex should NOT have been deleted")
+	}
+}
